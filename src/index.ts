06a10c2640b785df424ec16b858d896d912048e2
taskie <t@skie.jp>
2026-04-27 02:05:49 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import * as wasy from "./wasy.js";
 import * as midiIn from "./webmidi/midi-in.js";
 import * as midi from "./midi/event.js";
+import * as smf from "./smf.js";
 
 export {
     wasy,
     midiIn,
     midi,
+    smf,
 };
 
 export { Wasy } from "./wasy.js";
 export type { TimedEvent } from "./wasy.js";
 export { SmfPlayer } from "./smf-player.js";
-export { SynthEngine } from "./synth-engine.js";
+export { SynthEngine, isDrumChannel } from "./synth-engine.js";
