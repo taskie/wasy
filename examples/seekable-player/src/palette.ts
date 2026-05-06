5fa7ebb98e04006181870d5633c72911b5dcc126
taskie <t@skie.jp>
2026-05-06 09:48:32 +0900

    fix: foreground color in dark mode

--- examples/seekable-player/src/palette.ts
+++ examples/seekable-player/src/palette.ts
@@ -5,6 +5,8 @@ export const SOLARIZED = {
     base03: "#002b36",
     base02: "#073642",
     base01: "#586e75",
+    base00: "#657b83",
+    base0: "#839496",
     base1: "#93a1a1",
     base2: "#eee8d5",
     base3: "#fdf6e3",
